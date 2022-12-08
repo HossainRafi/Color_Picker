a860a32b7d2920d756307f1ab6b200b5da159bda
HossainRafi <hossainrafi71@gmail.com>
2022-12-08 19:24:56 +0600

    components

---
+++ src/components/ShadeInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShadeInput = () => {
+  return (
+    <div>ShadeInput</div>
+  )
+}
+
+export default ShadeInput
---
+++ src/components/Shades.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Shades = () => {
+  return (
+    <div>Shades</div>
+  )
+}
+
+export default Shades
