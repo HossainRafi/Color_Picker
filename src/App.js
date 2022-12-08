a3b7ce3c505643489f79bdce5140718875012429
HossainRafi <hossainrafi71@gmail.com>
2022-12-08 19:45:17 +0600

    send data as prop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 import ShadeInput from "./components/ShadeInput";
 import Shades from "./components/Shades";
 
 function App() {
+  const [color, setColor] = useState("#000");
+
+  const onInputChange = (value) => {
+    setColor(value);
+  };
   return (
     <div className="App">
-      <ShadeInput />
-      <Shades />
+      <ShadeInput color={color} onInputChange={onInputChange} />
+      <Shades color={color} />
     </div>
   );
 }
