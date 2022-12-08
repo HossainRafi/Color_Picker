f036a626bee116d4d6590e86597a81377601b855
HossainRafi <hossainrafi71@gmail.com>
2022-12-08 19:36:57 +0600

    shade input

--- src/components/ShadeInput.jsx
+++ src/components/ShadeInput.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const ShadeInput = () => {
   return (
     <div className="shade-search">
-      <input type="text" className="shade-input" />
+      <input
+        type="text"
+              className="shade-input"
+              placeholder="Type color name"
+      />
     </div>
   );
 };
