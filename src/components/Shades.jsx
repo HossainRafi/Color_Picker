45fc3ed92f603a29e3a1083c2df60c3bb8febeaa
HossainRafi <hossainrafi71@gmail.com>
2022-12-08 20:11:30 +0600

    get data from dependancy

--- src/components/Shades.jsx
+++ src/components/Shades.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Values from "values.js";
+import isColor from "is-color";
 
-const Shades = () => {
-  return (
-    <div className="shades">Shades</div>
-  )
-}
+const Shades = ({ color }) => {
+  useEffect(() => {
+    if (isColor(color)) createShades(color);
+  }, [color]);
 
-export default Shades
+  const createShades = (color) => {
+    const shades_color = new Values(color);
+    let shades = shades_color.shades(1);
+    console.log(shades[0].rgbString());
+  };
+
+  return <div className="shades"></div>;
+};
+
+export default Shades;
